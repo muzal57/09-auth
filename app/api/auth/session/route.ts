30635dfe7061d428c7f561f23bf8d4e48809a9b7
Serhii Muzalevskyi <seregamuza57@gmail.com>
2026-04-26 20:20:56 +0200

    hw

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -15,37 +15,34 @@ export async function GET() {
       return NextResponse.json({ success: true });
     }
 
-    if (!refreshToken) {
-      return NextResponse.json({ success: false }, { status: 200 });
-    }
+    if (refreshToken) {
+      const apiRes = await api.get("auth/session", {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+      });
 
-    const apiRes = await api.get("auth/session", {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    });
+      const setCookie = apiRes.headers["set-cookie"];
 
-    const setCookie = apiRes.headers["set-cookie"];
+      if (setCookie) {
+        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+        for (const cookieStr of cookieArray) {
+          const parsed = parse(cookieStr);
 
-    if (setCookie) {
-      const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-      for (const cookieStr of cookieArray) {
-        const parsed = parse(cookieStr);
+          const options = {
+            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+            path: parsed.Path,
+            maxAge: Number(parsed["Max-Age"]),
+          };
 
-        const options = {
-          expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-          path: parsed.Path,
-          maxAge: Number(parsed["Max-Age"]),
-        };
-
-        if (parsed.accessToken)
-          cookieStore.set("accessToken", parsed.accessToken, options);
-        if (parsed.refreshToken)
-          cookieStore.set("refreshToken", parsed.refreshToken, options);
+          if (parsed.accessToken)
+            cookieStore.set("accessToken", parsed.accessToken, options);
+          if (parsed.refreshToken)
+            cookieStore.set("refreshToken", parsed.refreshToken, options);
+        }
+        return NextResponse.json({ success: true }, { status: 200 });
       }
-      return NextResponse.json({ success: true }, { status: 200 });
     }
-
     return NextResponse.json({ success: false }, { status: 200 });
   } catch (error) {
     if (isAxiosError(error)) {
